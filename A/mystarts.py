4c7cd0f552059f49b2b435ef5595a0952b0ef1a3
hakutakaid <rachmadalfio@gmail.com>
2023-09-13 03:03:08 +0700

    hakutakaid

--- A/mystarts.py
+++ A/mystarts.py
@@ -2,7 +2,7 @@ from telethon import events, Button
 
 
 async def start_srb(event, st):
-    await event.reply(st,buttons=[[Button.url("OWNER", url="t.me/asukamuyaas")]])
+    await event.reply(st,buttons=[[Button.url("OWNER", url="t.me/can_you_silent_please")]])
 
     
 async def vc_menu(event):
@@ -12,5 +12,5 @@ async def vc_menu(event):
                          Button.inline("SOURCE", data="source")],
                         [Button.inline("NOTICE.", data="notice"),
                          Button.inline("Main.", data="help")],
-                        [Button.url("OWNER", url="t.me/asukamuyaas")]])
+                        [Button.url("OWNER", url="t.me/can_you_silent_please")]])
     
